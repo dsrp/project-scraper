598c045464cdaa4e37fabf5052d1e17940d88d68
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-02-13 16:44:13 +0000

    Auto-generated structure.

--- project_scraper/spiders/fic.py
+++ project_scraper/spiders/fic.py
@@ -1,11 +1,15 @@
-from scrapy.spiders import SitemapSpider
+# -*- coding: utf-8 -*-
+import scrapy
 
 
-class FicSpider(SitemapSpider):
+class FicSpider(scrapy.spiders.SitemapSpider):
+    name = 'fic'
+    allowed_domains = ['ic.org']
+
     sitemap_urls = [
         'https://www.ic.org/directory-sitemap1.xml',
         'https://www.ic.org/directory-sitemap2.xml'
     ]
 
     def parse(self, response):
-        pass # ... scrape item here ...
+        pass
--- project_scraper/spiders/gen.py
+++ project_scraper/spiders/gen.py
@@ -1,11 +1,15 @@
-from scrapy.spiders import SitemapSpider
+# -*- coding: utf-8 -*-
+import scrapy
 
 
-class GenSpider(SitemapSpider):
+class GenSpider(scrapy.spiders.SitemapSpider):
+    name = 'gen'
+    allowed_domains = ['ecovillage.org']
+
     sitemap_urls = [
         'https://ecovillage.org/gen_project-sitemap1.xml',
         'https://ecovillage.org/gen_project-sitemap2.xml'
     ]
 
     def parse(self, response):
-        pass # ... scrape item here ...
+        pass
