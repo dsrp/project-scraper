5831c380c7857e8aabec073325c272fcb9c3fce3
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-02-13 19:18:19 +0000

    Sidebar extraction working.

--- project_scraper/spiders/fic.py
+++ project_scraper/spiders/fic.py
@@ -11,19 +11,10 @@ class FicSpider(scrapy.spiders.SitemapSpider):
         'https://www.ic.org/directory-sitemap2.xml'
     ]
 
-    def parse(self, response):
-        main = response.css('div.directory-listing')
-
+    def parse_sidebar(self, main):
         sidebar = main.css('div.row.mb-2 > div.col-sm-10')
 
-        yield {
-            'title': main.css('h1::text').extract_first(),
-
-            # Main fields
-            'description': self.main_text(main, 'Community Description'),
-            'mission': self.main_text(main, 'Mission Statement'),
-
-            # Side bar
+        return {
             'status': self.side_field(sidebar, 'Status'),
             'started_planning': self.side_field(sidebar, 'Started Planning'),
             'started_living': self.side_field(sidebar, 'Start Living Together'),
@@ -34,6 +25,22 @@ class FicSpider(scrapy.spiders.SitemapSpider):
             'other_social': self.side_field(sidebar, 'Other social'),
             'address': self.side_field(sidebar, 'Community Address'),
         }
+
+    def parse_main(self, main):
+        return {
+            'title': main.css('h1::text').extract_first(),
+            'description': self.main_text(main, 'Community Description'),
+            'mission': self.main_text(main, 'Mission Statement'),
+
+        }
+
+    def parse(self, response):
+        main = response.css('div.directory-listing')
+        fields = self.parse_main(main)
+
+        fields.update(self.parse_sidebar(main))
+
+        yield fields
 
     def side_field(self, el, fieldname):
         """ Extracts fields from the right side. """
