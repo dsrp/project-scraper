1930d7cace332c19e585898554891f88c9c1bb6e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-02-13 17:47:44 +0000

    Started crawling FIC.

--- project_scraper/spiders/fic.py
+++ project_scraper/spiders/fic.py
@@ -12,4 +12,25 @@ class FicSpider(scrapy.spiders.SitemapSpider):
     ]
 
     def parse(self, response):
-        pass
+        main = response.css('#main')
+
+        yield {
+            'title': main.css('h1::text').extract_first(),
+            'description': self.parse_main_text(main, 'Community Description'),
+            'mission': self.parse_main_text(main, 'Mission Statement'),
+            'status': self.parse_side_field(main, 'Status')
+
+        }
+
+    def parse_side_field(self, main, fieldname):
+        """ Extracts fields from the right side. """
+        xpath = '//ul/li/text()[preceding::b[text()="{0}:"]]'.format(fieldname)
+        return main.xpath(xpath).extract_first().strip()
+
+    def parse_main_text(self, main, header):
+        """ Parse main header (h2) content, keeping HTML """
+        xpath = '//p[preceding::h2[text()="{0}"]]'.format(header)
+
+        text_nodes = main.xpath(xpath).extract()
+
+        return '\n'.join(text_nodes).strip()
