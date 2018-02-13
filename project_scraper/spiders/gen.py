38b2fd3be3b2e9da1cb27ae1d1b0cab80c54e22d
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-02-13 22:46:42 +0000

    Start work on GEN too.

--- project_scraper/spiders/gen.py
+++ project_scraper/spiders/gen.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from .utils import to_fieldname
+
 
 class GenSpider(scrapy.spiders.SitemapSpider):
     name = 'gen'
@@ -11,5 +13,35 @@ class GenSpider(scrapy.spiders.SitemapSpider):
         'https://ecovillage.org/gen_project-sitemap2.xml'
     ]
 
+    def parse_meta(self, wrapper):
+        """ Parse metadata from header. """
+
+        fields = {}
+        for el in wrapper.css('div.gen-project-entry-meta > div > span'):
+            name = to_fieldname(el.css('h4::text').extract_first())
+            value = el.xpath('./descendant-or-self::text()[position() > 1]').re_first(':*\s*(.*)\s*')
+
+            fields[name] = value
+
+        # TODO: planned_start, other_languages
+
+        return fields
+
+    def parse_sidebar(self, wrapper):
+        # TODO: email, social media, keywords, affiliated to, region, country,
+        # address, website, coordinates?
+        return {}
+
     def parse(self, response):
-        pass
+        w = response.css('article.gen_project')
+
+        fields = {
+            'title': w.css('span.entry-title-primary::text').extract_first(),
+            'subtitle': w.css('span.entry-subtitle::text').extract_first(),
+            'description': ''.join(w.css('div.project-description > *').extract())
+        }
+
+        fields.update(self.parse_meta(w))
+        fields.update(self.parse_sidebar(w))
+
+        yield fields
